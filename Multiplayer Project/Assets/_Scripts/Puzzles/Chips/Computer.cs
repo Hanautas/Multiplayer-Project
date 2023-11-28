eec735eb5982941f35b015e5f6d0dd043eacb6d0
dkemildk <dkemildk@gmail.com>
2023-11-28 13:47:39 +0100

    Computer chip physics movement

--- Multiplayer Project/Assets/_Scripts/Puzzles/Chips/Computer.cs
+++ Multiplayer Project/Assets/_Scripts/Puzzles/Chips/Computer.cs
@@ -6,11 +6,41 @@ public class Computer : MonoBehaviour
 {
     public ColorType type;
 
+    public Transform plugPosition;
+
+    public GameObject computerChip;
+
+    public bool canPickup = true;
+    public bool isConnected = false;
+    public bool isComplete = false;
+
+    private void Update()
+    {
+        if (isConnected && !canPickup)
+        {
+            computerChip.transform.position = plugPosition.position;
+            Vector3 eulerRotation = new Vector3(this.transform.eulerAngles.x + 90, this.transform.eulerAngles.y, this.transform.eulerAngles.z);
+            computerChip.transform.rotation = plugPosition.rotation;
+        }
+    }
+
+    private IEnumerator EnablePickup()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        canPickup = true;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "ComputerChip")
         {
-
+            isConnected = true;
+            computerChip = other.gameObject;
+            other.gameObject.transform.position = plugPosition.position;
+            other.gameObject.transform.rotation = plugPosition.rotation;
+            canPickup = false;
+            StartCoroutine(EnablePickup());
         }
     }
 }
