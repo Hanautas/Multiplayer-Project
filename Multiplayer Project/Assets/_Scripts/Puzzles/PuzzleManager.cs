da68cabe35daead79bb16e9acbd377dfe9be6a9a
emboni <128430325+dkemildk@users.noreply.github.com>
2023-12-04 14:34:38 +0100

    Timer added

--- Multiplayer Project/Assets/_Scripts/Puzzles/PuzzleManager.cs
+++ Multiplayer Project/Assets/_Scripts/Puzzles/PuzzleManager.cs
@@ -2,13 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-
+using TMPro;
 public class PuzzleManager : MonoBehaviour
 {
+    public bool isTimer = true;
+    public float time = 900f;
+    public float timeRemaining = 900f;
+    public TMP_Text timerText1;
+    public TMP_Text timerText2;
+    public TMP_Text timerText3;
     public List<Puzzle> puzzles;
-
     public UnityEvent completeEvent;
-
+    void Update()
+    {
+        if(isTimer)
+        {
+            if(timeRemaining > 0)
+            {
+                timeRemaining-= Time.deltaTime;
+                if(timeRemaining < 0)
+                {
+                    timeRemaining = 0;
+                }
+                string timeToDisplay = DisplayTime(timeRemaining);
+                timerText1.text = timeToDisplay;
+                timerText2.text = timeToDisplay;
+                timerText3.text = timeToDisplay;
+            }
+        }
+    }
+    string DisplayTime(float timeToDisplay)
+    {
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        return string.Format("{0:00}:{1:00}",minutes, seconds);
+    }
     public void CheckPuzzlesComplete()
     {
         foreach (Puzzle puzzle in puzzles)
